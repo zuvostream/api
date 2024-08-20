f9dcc794d8fd93cb9f93f61f0569f0e71abe1fa4
snowyluv <snowyfe@proton.me>
2024-08-19 22:47:42 -0500

    first commit

---
+++ src/index.ts
@@ -0,0 +1,69 @@
+import { Elysia, t } from "elysia";
+import { PrismaClient } from "@prisma/client"
+import { jwt } from '@elysiajs/jwt'
+import bcryptjs from "bcryptjs";
+let db = new PrismaClient();
+let app = new Elysia()
+.use(
+  jwt({
+    name: 'jwt',
+    secret: process.env.JWT!
+})
+)
+app.group('/v1', (app) => 
+  app
+  .post('/register', async ({body}) => {
+      let { username, email, password } = body as { username: string; email: string; password: string };
+      try {
+          let salt = await bcryptjs.genSalt(10);  
+          let hashedPassword = await bcryptjs.hash(password, salt); 
+          await db.user.create({
+              data: {
+                  username,
+                  email,
+                  password: hashedPassword
+              }
+          });
+          return { success: true, message: "User created successfully" };
+      } catch (error) {
+          return { success: false, message: "Error creating user"};
+      }
+  },
+  {
+      body: t.Object({
+          username: t.String({ maxLength: 32 }),
+          email: t.String({}),
+          password: t.String({ minLength: 8, maxLength: 32 })
+      })
+  })
+.post('/sigin', async ({body, jwt, cookie: { token }}) => {
+  let { username, password } = body as { username: string; password: string };
+  try {
+    let user = await db.user.findFirst({ 
+        where: { username }
+    });
+    if (!user || !(await bcryptjs.compare(password, user.password))) {
+        return { success: false, message: "Invalid identifier or password" };
+    } 
+    let jasontoken = await jwt.sign({ id: user.id  });
+    token.set({
+        value: jasontoken,
+        httpOnly: true,
+    });
+    return { success: true, token:  jasontoken};
+} catch (error) {
+    return { success: false, message: "Error during sign in" };
+}
+},
+{
+body: t.Object({
+    username: t.String({ maxLength: 32 }),
+    password: t.String({ minLength: 8 })
+})   
+})
+)
+.listen(process.env.PORT!);
+
+console.log(
+  `🦊 Elysia is running at http://${app.server?.hostname}:${app.server?.port}`
+);
